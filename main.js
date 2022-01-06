330bc900e3e7c4917325526512d1a25ee5a6ffda
Catrua163 <fml1630128@gmail.com>
2022-01-06 10:10:04 +0700

    BTVN7

---
+++ main.js
@@ -0,0 +1,7 @@
+alert("Bài số 2");
+let firstNumber = prompt("First number?", 1);
+let secondNumber = prompt("Second number?", 2);
+alert(firstNumber + secondNumber);
+alert("Bài số 3");
+let word = prompt("Nhập dữ liệu bất kỳ: ");
+alert("Số lượng kí tự vừa nhập: " + word.length);
